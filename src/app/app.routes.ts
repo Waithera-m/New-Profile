e287c5fbdf6930401cd82efbda5f35ef03ed45ea
Waithera <njihiamary11@gmail.com>
2024-12-25 20:08:24 +0300

    Init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: "welcome",
+        pathMatch: 'full'
+    },
+    {
+        path: "welcome",
+        loadComponent: () => 
+            import('./welcome-page/welcome-page.component').then(m => m.WelcomePageComponent)
+    }
+];
